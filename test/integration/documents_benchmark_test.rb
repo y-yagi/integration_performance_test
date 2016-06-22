0046d2cbb10b381a8a6946765ce7b23fe68a31da
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-06-23 08:38:46 +0900

    add integration only benchmark script

---
+++ test/integration/documents_benchmark_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+require 'benchmark/ips'
+
+class DocumentsIntegrationTest < ActionDispatch::IntegrationTest
+  test "index" do
+    get '/documents'
+    assert_equal 200, response.status
+  end
+
+  test "create" do
+    post '/documents', params: { document: { title: "New things", content: "Doing them" } }
+
+    document = Document.last
+    assert_equal 'New things', document.title
+    assert_equal 'Doing them', document.content
+  end
+end
+
+Benchmark.ips(5) do |bm|
+  bm.report 'INDEX: Integration Test' do
+    Minitest.run_one_method(DocumentsIntegrationTest, 'test_index')
+  end
+end
+
+Benchmark.ips(5) do |bm|
+  bm.report 'CREATE: Integration Test' do
+    Minitest.run_one_method(DocumentsIntegrationTest, 'test_create')
+  end
+end
